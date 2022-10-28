288b47aa043e677f5cfbfccc0d882aefdf0c572b
matt-campos <matt-campos@users.noreply.github.com>
2022-10-28 10:56:44 -0500

    creating repo
    adding coding task skeleton project

---
+++ AppCode.Dev/DevStartup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AppCode.Dev.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppCode.Dev
+{
+
+    public class DevStartup: Startup
+    {
+        // usually the configuration will be injected, but we're not doing that by default in Startup
+        // so we'll have here commented for convenience
+        // public DevStartup(IConfiguration configuration): base(configuration)
+        // {  
+        // }
+
+        public override void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            base.Configure(app, env, loggerFactory);
+
+            #region Swagger
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3(settings =>
+            {
+                settings.DocumentTitle = "Appcode Assessment Swagger";
+                settings.Path = $"{SwaggerRoutes.Controller}";
+                settings.DocumentPath = $"{SwaggerRoutes.Controller}/{SwaggerRoutes.JsonSpec}";
+            });
+            app.UseReDoc(settings =>
+            {
+                settings.Path = $"{SwaggerRoutes.DocBase}/redoc";
+                settings.DocumentPath = $"{SwaggerRoutes.Controller}/{SwaggerRoutes.JsonSpec}";
+            });
+            
+            #endregion
+        }
+    }
+}
---
+++ AppCode/Startup.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+
+namespace AppCode
+{
+    public class Startup
+    {
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app
+                .UseRouting()
+                .UseEndpoints(endPoints => { endPoints.MapControllers(); });
+        }
+    }
+}
